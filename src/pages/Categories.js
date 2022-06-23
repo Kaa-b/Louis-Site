5faf937a98ce7eff9f2286dc2d01ad34bf129fa3
Kaa-b <101167755+Kaa-b@users.noreply.github.com>
2022-06-23 08:24:20 +0200

    Add files via upload

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { Wrapper, Content } from "./Categories.styles";
-import {images} from "../data";
+import { images } from "../data";
 //Components
 import Grid from "../components/Grid";
 import Underline from "../components/Underline";
@@ -10,19 +10,33 @@ import Navbar from "../components/Navbar";
 import CategorieHeader from "../components/CategorieHeader";
 
 const Categories = () => {
-
   const { categories } = useParams();
 
+  let liste = [];
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].categorie.includes(`${categories}`)) {
+      liste.push(images[i]);
+    }
+  }
   return (
     <Wrapper>
       <Navbar />
       <Content>
-      <CategorieHeader categorie={categories}/>
-      <Underline />
+        <CategorieHeader categorie={categories} />
+        <Underline />
         <Grid header="Début grille">
-        {images.map((image) => {
+          {liste.map((image) => {
             const { id, description, url, categorie } = image;
-            return <Thumb key={id} url={url} categorie={categorie} description={description} clickable={false} />;
+
+            return (
+              <Thumb
+                key={id}
+                url={url}
+                categorie={categorie}
+                description={description}
+                clickable={false}
+              />
+            );
           })}
         </Grid>
       </Content>
